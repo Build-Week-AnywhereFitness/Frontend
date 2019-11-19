5020e59bd322424395e7d8e8a1666b5e7d5c6d47
avi <avikan1087@gmail.com>
2019-11-19 01:36:22 -0500

    register and part of nav

---
+++ anywhere_fitness/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Register from './components/Register';
+import Navigation from './components/Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Register/>
+    </div>
+  );
+}
+
+export default App;
