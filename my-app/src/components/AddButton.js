67f0d63a2077642e1a27fa2250ca322fcab55b4a
Matthew Cebenka <matthewt.cebenka@gmail.com>
2019-11-19 18:26:32 -0500

    Added NavBar w/ search field and log out button as well as the Add-Session button and card that will be dynamically rendered

---
+++ my-app/src/components/AddButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+
+    const useStyles = makeStyles(theme => ({
+    fab: {
+        margin: theme.spacing(1),
+    },
+    extendedIcon: {
+        marginRight: theme.spacing(1),
+    },
+    }));
+
+    export default function FloatingActionButtons() {
+    const classes = useStyles();
+
+    return (
+        <div>
+        <Fab variant="extended" aria-label="delete" className={classes.fab}>
+            <AddIcon className={classes.extendedIcon} />
+            Add Session
+        </Fab>
+        </div>
+    );
+    }
