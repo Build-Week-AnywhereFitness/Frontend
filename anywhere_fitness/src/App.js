26d7925387a4fb8b67bbdc7cf3dff3539e9b8eee
Mel Maki <melody.rackham@gmail.com>
2019-11-20 10:25:23 -0700

    Updated routes on app.js

--- anywhere_fitness/src/App.js
+++ anywhere_fitness/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import {Route} from "react-router-dom";
+import { Route } from 'react-router-dom';
 import CoachDashBoard from './components/CoachDashBoard';
 import Login from './components/Login';
+import ClientDashList from './components/ClientDashList';
+import NewUser from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path="/" component={Login}/>
-      <Route path="/coachdashboard" component={CoachDashBoard}/>
+    <div className='App'>
+      <Route exact path='/' component={Login} />
+      <Route path='/Register' component={NewUser} />
+      <Route path='/coachdashboard' component={CoachDashBoard} />
+      <Route path='/ClientDashList' component={ClientDashList} />
     </div>
   );
 }
