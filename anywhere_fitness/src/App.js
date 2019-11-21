fd4e03298ccbf1540060039ac43f04debb84f8b1
Mel Maki <melody.rackham@gmail.com>
2019-11-21 10:23:13 -0700

    Built out state management using Context API

--- anywhere_fitness/src/App.js
+++ anywhere_fitness/src/App.js
@@ -6,16 +6,24 @@ import Login from './components/Login';
 import ClientDashList from './components/ClientDashList';
 import NewUser from './components/Register';
 import PrivateRoute from './utils/PrivateRoute';
+import CoachContext from './contexts/CoachContext';
+import ClientContext from './contexts/ClientContext';
 
 function App() {
-  const [data, setData] = useState('');
+  const [coaches, setCoaches] = useState('');
+  const [clients, setClients] = useState([]);
+
   return (
-    <div className='App'>
-      <Route exact path='/' component={Login} />
-      <Route path='/Register' component={NewUser} />
-      <PrivateRoute path='/CoachDashBoard' component={CoachDashBoard} />
-      <PrivateRoute path='/ClientDashList' component={ClientDashList} />
-    </div>
+    <CoachContext.Provider value={{ coaches }}>
+      <ClientContext.Provider value={{ clients }}>
+        <div className='App'>
+          <Route exact path='/' component={Login} />
+          <Route path='/Register' component={NewUser} />
+          <PrivateRoute path='/CoachDashBoard' component={CoachDashBoard} />
+          <PrivateRoute path='/ClientDashList' component={ClientDashList} />
+        </div>
+      </ClientContext.Provider>
+    </CoachContext.Provider>
   );
 }
 
